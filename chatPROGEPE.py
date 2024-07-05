5a38839c80572c545f9bab17e94f81b42b9a5d8c
legfjr-github <146836616+legfjr-github@users.noreply.github.com>
2024-07-05 15:07:45 -0300

    Update chatPROGEPE.py

--- chatPROGEPE.py
+++ chatPROGEPE.py
@@ -40,7 +40,7 @@ span{visibility: hidden;}
 
 st.markdown('<img style="float: left; padding-right: 3%;" width=150px src="https://www.ufpe.br/documents/20181/60718/Progepe-100px-margem.png/cb408fe1-7c38-4cef-9619-cb861e8f5310?t=1471545541049" /><h1>Assistente PROGEPE</h1>', unsafe_allow_html=True)
 
-llm = ChatGoogleGenerativeAI(model="gemini-1.5-pro")
+llm = ChatGoogleGenerativeAI(model="gemini-1.5-flash")
 
 credential_file = "temp.json"
 sheet_title = "1.ProgepeChat"
