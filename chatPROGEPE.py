2848557edee4c3d8db42627529e4d497af7d6c9b
legfjr-github <146836616+legfjr-github@users.noreply.github.com>
2024-07-10 12:18:02 -0300

    Update chatPROGEPE.py

--- chatPROGEPE.py
+++ chatPROGEPE.py
@@ -84,7 +84,7 @@ if prompt := st.chat_input("Digite sua dúvida..."):
         if "api" not in st.session_state:
             st.session_state.api = cont
 
-    api_key = os.getenv(f'key{st.session_state.api%35}')
+    api_key = st.secrets[f'key{st.session_state.api%35}']
     pergunta += st.session_state.diff + "\n" + "--Início do Chat--\n"
     
     llm = ChatGoogleGenerativeAI(model="gemini-1.5-flash",google_api_key=api_key)
